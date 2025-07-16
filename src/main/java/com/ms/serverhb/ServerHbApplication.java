df19148f4a039ba92cc4f4b31f183362a1fef8c9
Michail19 <Mi.ershov.2017@yandex.ru>
2025-07-16 19:11:05 +0300

    Initial commit

---
+++ src/main/java/com/ms/serverhb/ServerHbApplication.java
@@ -0,0 +1,13 @@
+package com.ms.serverhb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerHbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerHbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/serverhb/ServerHbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.serverhb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerHbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
